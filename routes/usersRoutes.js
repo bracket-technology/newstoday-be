b2af6054a49017067b76c2130472d5f447650c82
dhanisetiaji <dhanisetiaji99@gmail.com>
2022-07-22 18:04:35 +0700

    feat:add middleware

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -2,26 +2,14 @@ const express = require("express")
 const router = express.Router()
 const userUploads = require("../helpers/userUploads")
 const { getAllUsers, getEmail, updateAdmin, updatePassword, updateUser, deleteUser } = require('../controllers/usersControllers')
+const { isLogin, isAdmin } = require('../middlewares/auth')
 
-router.get('/', getEmail)
-router.patch('/:userId', userUploads, updateUser)
-router.patch('/password/:userId', updatePassword)
+router.get('/', isLogin, getEmail)
+router.patch('/:userId', isLogin, userUploads, updateUser)
+router.patch('/password/:userId', isLogin, updatePassword)
 
-router.get('/admin', getAllUsers)
-router.patch('/admin/:userId', userUploads, updateAdmin)
-router.delete('/admin/:userId', deleteUser)
-
-module.exports = router
-
-
-
-
-
-
-
-
-
-
-
+router.get('/admin', isAdmin, getAllUsers)
+router.patch('/admin/:userId', isAdmin, userUploads, updateAdmin)
+router.delete('/admin/:userId', isAdmin, deleteUser)
 
 module.exports = router
