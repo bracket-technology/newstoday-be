87e2d59e30f158a04cd50f4b4a68192c34b1fbfa
Novianaa <noviana.rahmawati80@gmail.com>
2022-07-24 00:27:22 +0700

    feat: add modul comment

--- routes/commentsRoutes.js
+++ routes/commentsRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const router = express.Router()
-const { addComments, getAllByNewsId } = require('../controllers/commentsController')
+const { addComments, getAllByNewsId, getCommentsByUserId, deleteComment } = require('../controllers/commentsController')
 
 router.post('/', addComments)
-
+router.get('/:userId', getCommentsByUserId)
+router.delete('/:commentId', deleteComment)
+router.get('/article/:newsId', getAllByNewsId)
 
 module.exports = router
