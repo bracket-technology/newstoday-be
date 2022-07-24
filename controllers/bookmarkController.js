d503b1c7f5efb2bb04e9321636bbfa0e4b4784b6
Ahmad Wahyudi <108715816+ahmadvvahyudi@users.noreply.github.com>
2022-07-24 22:09:44 +0700

    feat: add bookmark controller, routes, models

--- controllers/bookmarkController.js
+++ controllers/bookmarkController.js
@@ -1,2 +1,48 @@
-const Bookmarks = require('../models/bookmarks')
+const {getAllBookmark, getBookmarkByUsers, addBookmark, updateBookmark, deleteBookmark} = require('../models/bookmarks')
 
+const getAll = async (req, res) => {
+    try {
+        const results = await getAllBookmark(req, res)
+        return res.status(200).send(results)
+    } catch (error) {
+        return res.status(500).send(error)
+    }
+}
+
+const getByUsers = async (req, res) => {
+    try {
+        const results = await getBookmarkByUsers(req, res)
+        return res.status(200).send(results)
+    } catch (error) {
+        return res.status(500).send(error)
+    }
+}
+
+const add = async (req, res) => {
+    try {
+        const results = await addBookmark(req, res)
+        return res.status(200).send(results)
+    } catch (error) {
+        return res.status(500).send(error)
+    }
+}
+
+const update = async (req, res) => {
+    try {
+        const results = await updateBookmark(req, res)
+        return res.status(200).send(results)
+    } catch (error) {
+        return res.status(500).send(error)
+    }
+}
+
+const deleteBookmark = async (req, res) => {
+    try {
+        const results = await deleteBookmark(req, res)
+        return res.status(200).send(results)
+    } catch (error) {
+        return res.status(500).send(error)
+    }
+}
+
+module.exports = {getAll, getByUsers, add, update, deleteBookmark}
--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,14 @@ const app = express()
 const usersRoutes = require('./usersRoutes')
 const authRoutes = require('./authRoutes')
 const categoryRoutes = require('./categoryRoutes')
+const bookmarkRoutes = require('./bookmarkRoutes')
 const {isLogin, isAdmin } = require('../middlewares/auth')
 
 
 app.use('/users',isLogin, usersRoutes)
 app.use('/auth', authRoutes)
 app.use('/category',isLogin,isAdmin, categoryRoutes)
+app.use('/bookmark',isLogin, bookmarkRoutes)
 
 
 
