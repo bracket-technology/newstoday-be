fcfc5dbd2bd3ccaab1c39bb60b98e9b1ae81568c
dhanisetiaji <dhanisetiaji99@gmail.com>
2022-08-25 01:04:36 +0700

    typo

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,14 +1,14 @@
 const express = require("express")
 const router = express.Router()
 const categoryUploads = require("../helpers/categoryUploads")
-const { getAllCategory, getCategoryById, addCategory, updateCategory, deleteCategory } = require('../controllers/categoryControllers')
+const { getAllCategory, getCategoryById, addCategory, updateCategory, deleteCategory } = require('../controllers/categoryController')
 const { isAdmin } = require('../middlewares/auth')
 
 
 router.get('/', getAllCategory)
 router.get('/:categoryId', getCategoryById)
-router.post('/',isAdmin, categoryUploads, addCategory)
-router.patch('/:categoryId',isAdmin, categoryUploads, updateCategory)
-router.delete('/:categoryId',isAdmin, deleteCategory)
+router.post('/', isAdmin, categoryUploads, addCategory)
+router.patch('/:categoryId', isAdmin, categoryUploads, updateCategory)
+router.delete('/:categoryId', isAdmin, deleteCategory)
 
 module.exports = router
